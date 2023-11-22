0897e3da95974dbf4b480187397ca199d68b7ee9
Yash-Htek <yash.gajbhiye@hoonartek.com>
2023-11-22 18:25:06 +0530

    f4

--- main.tf
+++ main.tf
@@ -16,6 +16,10 @@ terraform {
 }
 
 provider "snowflake" {
+  username   = dotenv.get("SNOWFLAKE_USER")
+  password   = dotenv.get("SNOWFLAKE_PASSWORD")
+  account    = dotenv.get("SNOWFLAKE_ACCOUNT")
+  region     = dotenv.get("SNOWFLAKE_REGION")
 }
 
 resource "snowflake_database" "demo_db" {
